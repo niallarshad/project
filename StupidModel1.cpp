df37f5577cbea7fd37f546f7fa507455203f8af1
niall <niallarshad001@hotmail.com>
2014-11-04 18:40:25 +0000

    Phase one done

--- StupidModel1.cpp
+++ StupidModel1.cpp
@@ -22,6 +22,14 @@
 #include <iostream>
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include <vector>
+
+#define GridSize 100
+#define BugCount 100
+#define BugVision 4
+#define GridSpacing 5
+
+using namespace sf;
  
  
 ////////////////////////////////////////////////////////////
@@ -30,52 +38,97 @@
 
 struct Cell {
 	bool hasBug;
-}
+
+	Cell() : hasBug(false) {}
+};
 
 class Bug {
 private:
-	sf::CircleShape m_shape;
+	//the shape to draw as the bug
+	CircleShape m_shape;
+
+	//the location of bug in grid
 	Vector2i gridLoc;
-	vector<vector<Cell>> * grid;
+
+	//pointer to main grid
+	std::vector<std::vector<Cell>> * grid;
+
 public:
-	Bug(const sf:CircleShape &shape, vector<vector<Cell>> * grid) : m_shape(shape), grid(grid) {}
+	Bug(Vector2i loc, float radius, std::vector<std::vector<Cell>> * grid) : gridLoc(loc), m_shape(radius), grid(grid) {
+		m_shape.setPosition(gridLoc.x * GridSpacing, gridLoc.y * GridSpacing);
+		m_shape.setFillColor(Color::Red);
+	}
+	
 	void Move() {
-		Vector2i desiredLoc(rand() % 9 - 4, rand() % 9 - 4);
 
-		while( grid->at(desiredLoc.x).at(desiredLoc.y).hasBug )
+		Vector2i desiredLoc;
+
+		do
 		{
+			//the location the bug wants to move to
 			desiredLoc = Vector2i(rand() % 9 - 4, rand() % 9 - 4);
-		}
-		
+
+			desiredLoc = gridLoc - desiredLoc;
+
+			if(desiredLoc.x < 0)
+				desiredLoc.x += 100;
+			else if(desiredLoc.x >= 100)
+				desiredLoc.x -= 100;
+
+			if(desiredLoc.y < 0)
+				desiredLoc.y += 100;
+			else if(desiredLoc.y >= 100)
+				desiredLoc.y -= 100;
+
+		} while ( grid->at(desiredLoc.x).at(desiredLoc.y).hasBug );	//while there is already a bug at the desired location
+
+		grid->at(gridLoc.x).at(gridLoc.y).hasBug = false;
+		grid->at(desiredLoc.x).at(desiredLoc.y).hasBug = true;
+
+		//move to the desired location
 		gridLoc = Vector2i(desiredLoc);
+
+		m_shape.setPosition(gridLoc.x * GridSpacing, gridLoc.y * GridSpacing);
 	}
 
-	void Draw(sf::RenderWindow const & w) {
-		w.Draw(m_shape);
+	//tell the render window to draw the shape
+	void Draw(sf::RenderWindow &w) {
+		w.draw(m_shape);
 	}
-}
+};
  
 int main()
 {
+	srand( time(NULL) );
+
 	// Create the main window
-	sf::RenderWindow window(sf::VideoMode(800, 600, 32), "Stupid model 1");
-	const int gridSize = 100;
-	const int bugCount = 100;
-	const int bugVision = 4;
+	RenderWindow window(sf::VideoMode(800, 600, 32), "Stupid model 1");
 		 
-	vector<vector<Cell>> grid;
-	vector<Bug> bugs;
-	
-	for(auto itr = grid.begin(); itr != grid.end(); itr++)
-	{
-		for(auto itr2 = itr->begin(); itr2 != itr->end(); itr++)
-		{
-			*itr2 = Cell();
-			itr2->hasBug = false();
+	std::vector<std::vector<Cell>> grid;
+	std::vector<Bug> bugs;
+
+	//fill the vectors with cells
+	for( int x = 0; x < GridSize; x++ ) {
+
+		grid.push_back( std::vector<Cell>() );
+
+		for( int y = 0; y < GridSize; y++ ) {
+
+			grid.at( x ).push_back( Cell() );
+
 		}
 	}
 
+	//place bugs randomly in the grid
+	for (int i = 0; i < BugCount; i++) {
+		int x = rand() % GridSize,
+			y = rand() % GridSize;
 
+		grid[x][y].hasBug = true;
+		bugs.push_back( Bug(Vector2i(x,y), 1, &grid) );
+	}
+
+	Clock clock = Clock();
 
 	while (window.isOpen())
 	{
@@ -92,6 +145,18 @@ int main()
 		}
 		//prepare frame
 		window.clear();
+
+		//move bugs once a second
+		if ( clock.getElapsedTime().asSeconds() >= 1 ) {
+
+			for(auto itr = bugs.begin(); itr != bugs.end(); itr++)
+			{
+				itr->Move();
+			}
+
+			//restart the clock
+			clock.restart();
+		}
 		
 		for(auto itr = bugs.begin(); itr != bugs.end(); itr++)
 		{
